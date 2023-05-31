9e0b6a2a0523f68759f6cbfa126eea5609d9bee1
mitchell <70453897+orbitalquark@users.noreply.github.com>
2023-05-31 19:12:06 -0400

    Reformat source code to use tabs for indentation. Requires some bugfixes to LuaFormatter's handling of tabs.

--- init.lua
+++ init.lua
@@ -20,8 +20,8 @@ if not rawget(_L, 'Open file:') then _L['Open file:'] = 'Open file:' end
 --- Normalizes a Windows path by replacing '/' with '\\'.
 -- Also transforms Cygwin-style '/c/' root directories into 'C:\'.
 local function win32_normalize(path)
-  return path:gsub('^/([%a])/', function(ch) return string.format('%s:\\', string.upper(ch)) end)
-    :gsub('/', '\\')
+	return path:gsub('^/([%a])/', function(ch) return string.format('%s:\\', string.upper(ch)) end)
+		:gsub('/', '\\')
 end
 
 --- Opens the command entry in a mode that can open files relative to the current file or
@@ -31,42 +31,42 @@ end
 -- Open dialog.
 -- @function _G.ui.command_entry.open_file
 local function open_file()
-  ui.command_entry.run(_L['Open file:'], function(file)
-    if file ~= '' and not file:find('^%a?:?[/\\]') then
-      -- Convert relative path into an absolute one.
-      file = (buffer.filename or lfs.currentdir() .. '/'):match('^.+[/\\]') .. file
-    end
-    if WIN32 then file = win32_normalize(file) end
-    io.open_file(file ~= '' and file or nil)
-  end, {
-    ['\t'] = function()
-      if ui.command_entry:auto_c_active() then return end
-      -- Autocomplete the filename in the command entry
-      local files = {}
-      local path = ui.command_entry:get_text()
-      if not path:find('^%a?:?[/\\]') then
-        -- Convert relative path into an absolute one.
-        path = (buffer.filename or lfs.currentdir() .. '/'):match('^.+[/\\]') .. path
-      end
-      if WIN32 then path = win32_normalize(path) end
-      local dir, part = path:match('^(.-)\\?([^/\\]*)$')
-      if WIN32 and dir:find('^%a:$') then dir = dir .. '\\' end -- C: --> C:\
-      if lfs.attributes(dir, 'mode') == 'directory' then
-        -- Iterate over directory, finding file matches.
-        local patt = '^' .. part:gsub('(%p)', '%%%1')
-        for filename in lfs.walk(dir, nil, 0, true) do
-          filename = filename:match('[^/\\]+[/\\]?$')
-          if filename:find(patt) then files[#files + 1] = filename end
-        end
-        table.sort(files)
-        local sep = ui.command_entry.auto_c_separator
-        ui.command_entry.auto_c_separator = string.byte(';')
-        ui.command_entry.auto_c_order = buffer.ORDER_PRESORTED
-        ui.command_entry:auto_c_show(#part, table.concat(files, ';'))
-        ui.command_entry.auto_c_separator = sep -- restore
-      end
-    end
-  })
+	ui.command_entry.run(_L['Open file:'], function(file)
+		if file ~= '' and not file:find('^%a?:?[/\\]') then
+			-- Convert relative path into an absolute one.
+			file = (buffer.filename or lfs.currentdir() .. '/'):match('^.+[/\\]') .. file
+		end
+		if WIN32 then file = win32_normalize(file) end
+		io.open_file(file ~= '' and file or nil)
+	end, {
+		['\t'] = function()
+			if ui.command_entry:auto_c_active() then return end
+			-- Autocomplete the filename in the command entry
+			local files = {}
+			local path = ui.command_entry:get_text()
+			if not path:find('^%a?:?[/\\]') then
+				-- Convert relative path into an absolute one.
+				path = (buffer.filename or lfs.currentdir() .. '/'):match('^.+[/\\]') .. path
+			end
+			if WIN32 then path = win32_normalize(path) end
+			local dir, part = path:match('^(.-)\\?([^/\\]*)$')
+			if WIN32 and dir:find('^%a:$') then dir = dir .. '\\' end -- C: --> C:\
+			if lfs.attributes(dir, 'mode') == 'directory' then
+				-- Iterate over directory, finding file matches.
+				local patt = '^' .. part:gsub('(%p)', '%%%1')
+				for filename in lfs.walk(dir, nil, 0, true) do
+					filename = filename:match('[^/\\]+[/\\]?$')
+					if filename:find(patt) then files[#files + 1] = filename end
+				end
+				table.sort(files)
+				local sep = ui.command_entry.auto_c_separator
+				ui.command_entry.auto_c_separator = string.byte(';')
+				ui.command_entry.auto_c_order = buffer.ORDER_PRESORTED
+				ui.command_entry:auto_c_show(#part, table.concat(files, ';'))
+				ui.command_entry.auto_c_separator = sep -- restore
+			end
+		end
+	})
 end
 rawset(ui.command_entry, 'open_file', open_file)
 
