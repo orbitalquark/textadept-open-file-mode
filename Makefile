d2bb1b404be4c8c8674cd3bfd934078b859fc404
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2025-01-01 10:23:00 -0500

    Updated copyright year.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright 2019-2024 Mitchell. See LICENSE.
+# Copyright 2019-2025 Mitchell. See LICENSE.
 
 # Documentation.
 
--- init_test.lua
+++ init_test.lua
@@ -1,4 +1,4 @@
--- Copyright 2020-2024 Mitchell. See LICENSE.
+-- Copyright 2020-2025 Mitchell. See LICENSE.
 
 local open_file_mode = require('open_file_mode')
 
